367f7df46b9273eb84c9c0cad3b6b4a68ed1d6ad
srijaytuladhar <srijay73@gmail.com>
2021-06-14 12:10:33 +0545

    second commit

--- src/main/java/com/spring/restapi/restapi/RestapiApplication.java
+++ src/main/java/com/spring/restapi/restapi/RestapiApplication.java
@@ -1,7 +1,11 @@
 package com.spring.restapi.restapi;
 
+import com.spring.restapi.restapi.filter.RequestResponseFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestapiApplication {
 
+	private static final String httpUrl = "/students/*";
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
@@ -19,5 +26,17 @@ public class RestapiApplication {
 	@RequestMapping(value = "/")
 	public ResponseEntity<Object> homePage() {
 		return new ResponseEntity<>("Homepage", HttpStatus.OK);
+	}
+
+
+	@Bean
+	public FilterRegistrationBean<RequestResponseFilter> loggingFilter(){
+		FilterRegistrationBean<RequestResponseFilter> registrationBean
+				= new FilterRegistrationBean<>();
+
+		registrationBean.setFilter(new RequestResponseFilter());
+		registrationBean.addUrlPatterns(httpUrl);
+
+		return registrationBean;
 	}
 }
--- src/main/java/com/spring/restapi/restapi/filter/RequestResponseFilter.java
+++ src/main/java/com/spring/restapi/restapi/filter/RequestResponseFilter.java
@@ -4,22 +4,18 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
 
-public class TransactionFilter implements Filter {
-
-    Logger logger;
-
+public class RequestResponseFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
 
-        System.out.println("Starting a transaction for req : localhost:8080 " + req.getRequestURI());
-
+        System.out.println( "Logging Request: " + req.getMethod() + req.getRequestURI() + " <--------------- Method Mapping");
 
         filterChain.doFilter(servletRequest, servletResponse);
-        System.out.println("Committing a transaction for req : localhost:8080" +  req.getRequestURI());
+        System.out.println("Logging Response: " + res.getContentType() + " <--------------- Header Type");
     }
 }
